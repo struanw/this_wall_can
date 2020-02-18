46f00479c1feb940034446aeef41c282a0a3d7ec
Struan-Wall <struan03@hotmail.com>
2020-02-18 16:00:07 +1300

    add some future compnents

---
+++ client/components/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+
+
+class About extends React.Component {
+
+
+
+render () {
+    return (
+        <>
+  <div className="example">
+    <h4><b>About</b></h4>
+    <div class="content">
+    <div className="text"> I am a full stack developer interested in building  immersive user experiences. While living in Amsterdam, NL, and spending some time visiting a friend's digital production company, I realised I wanted to work in a creative development environment. I am an engineer and recently completed a web dev boot camp and a compsci certificate. I am looking for a junior front end role where I can grow as a developer and contribute to a team. my goal is to become a proficience developer ababting to various build enviorments and languages and working becoming a team lead. This app was built to try out various JS techniques and libraries using React. Familiarity with JS, React, Redux, Git, C, Python, SQL, Node, Sumologic, Jira, Rundeck. The photo of me was taken in Christchurch CBD. Hobbies: Surfing, Building a tiny house and rebuilding my Morris Mini</div>
+  </div>
+</div>
+
+
+        </> 
+       
+      )
+    }
+}
+export default About
+
+
